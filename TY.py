6e9126a041a243dd11c46b4e6f191d8f1f50c85b
sxsdkk <sxsdkk@sina.com>
2018-03-14 18:03:13 +0800

    Add files via upload

---
+++ TY.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import  requests
+url = 'http://sports.sina.cn'
+r = requests.get(url)
+print(r.encoding)
+data = r.text
+print(data.encode('latin1').decode('utf-8'))
